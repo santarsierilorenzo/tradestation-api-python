ca299a7cface48188b3ebbcfba57675bacd17fbd
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2025-11-09 17:38:14 +0000

    examples for users: streams

---
+++ examples/get_stream_bars.py
@@ -0,0 +1,29 @@
+from src.client import TradeStationClient
+from src.auth import TokenManager
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    # Creare un istanza condivisa di TokenManager.
+    token_manager = TokenManager()
+
+    # Facade Pattern, TradeStationClient is an entry point.
+    ts_client = TradeStationClient(
+        token_manager=token_manager
+    )
+
+    ts_client.market_data_stream.stream_bars(
+        symbol="AAPL",
+        interval=1,
+        unit="Minute",
+        barsback=5,
+    )
+
+
